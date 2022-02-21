d4a8c2fb559f70177f81ff0835913d56f0f810a5
RyukiMurthi <99290844+RyukiMurthi@users.noreply.github.com>
2022-02-22 11:00:52 +1300

    Add files via upload

--- 00_MMF_base.py
+++ 00_MMF_base.py
@@ -18,3 +18,45 @@ def not_blank (question):
 
 #get name (can't be blank)
 name = not_blank("Name: ")
+
+
+
+
+
+
+
+
+# loop to get details
+
+# start of loop
+
+# initialise loop so that it runs at least once
+from typing import Mapping
+
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+    
+    # tells user how many setas are left
+    if count < 4:
+        print ("You have {} seats left".format (MAX_TICKETS - count))
+
+    # warns user that only one seat is left
+    else:
+        print("*** There is 1 seat left! ***")
+
+    # get details...
+
+    #get name (can't be blank)
+    name = not_blank("Name: ")
+    count += 1
+    print ()
+
+if count == MAX_TICKETS:
+    print ("You have sold all the available tickets!")
+else:
+    print ("You have sold {} tickets.   \n"
+        "There are {} places still availabe".format (count, MAX_TICKETS - count))
--- 02_ticket_loop_v2.py
+++ 02_ticket_loop_v2.py
@@ -1,13 +1,22 @@
 # start of loop
 
 # initialise loop so that it runs at least once
+from typing import Mapping
+
+
 name = ""
 count = 0
 MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
-    print ("You have {} seats left".format ( MAX_TICKETS - count))
+    
+    # tells user how many setas are left
+    if count < 4:
+        print ("You have {} seats left".format (MAX_TICKETS - count))
 
+    # warns user that only one seat is left
+    else:
+        print("*** There is 1 seat left! ***")
 
     # get details...
     name = input ("name: ")
