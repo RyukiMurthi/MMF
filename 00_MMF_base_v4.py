82ba2073358ea565ebc00215062d692717bc9a93
RyukiMurthi <99290844+RyukiMurthi@users.noreply.github.com>
2022-03-22 10:39:39 +1300

    Add files via upload
    surcharge

--- 00_MMF_base_v4.py
+++ 00_MMF_base_v4.py
@@ -80,7 +80,101 @@ def get_ticket_price():
 
     return ticket_price
 
+
+def string_check (choice, options):
+
+    is_valid = ""
+    chosen = ""
+
+    for var_list in options:
+
+        # if the snaclk is in one of the lists, return the full statement
+        if choice in var_list:
+
+            # get full name of snack and put it
+            #in title case so it looks nice when outputted
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+        
+        # if the chosen option is not valid, set is_valid to no
+        else:
+            is_valid = "no"
+
+    # if the snack is not OK - ask question again
+    if is_valid == "yes":
+        return chosen
+    else:
+        return "invalid choice"
+
+
+def get_snack ():
+
+    # regular expression to find if item starts with a number
+    number_regex = "[1-9]"
+
+    # valid snacks holds list of all snacks
+    # each item in valid snacks is a list with
+    # valid options for each snack <full name, letter code (a - e)
+    # and possible abreviations etc>
+    valid_snacks = [
+        ["popcorn", "p", "corn", "a"],
+        ["M&M's", "m&m's", "mms", "m", "b"],
+        ["pita chips", "chips", "pc", "pita", "c"],
+        ["water", "w", "d"],
+        ["orange juice", "oj", "o", "juice", "e"]
+    ]
+
+    # holds snack order for a single user
+    snack_order = []
+
+    desired_snack = ""
+    while desired_snack != "xxx":
+        
+        snack_row = []
+        
+         # ask user for desired snack and put it in lower case
+        desired_snack = input ("Snack: ").lower()
+
+        if desired_snack == "xxx":
+            return snack_order
+
+        # if item has a number, separate it into two (number / item)
+        if re.match (number_regex, desired_snack):
+            amount = int (desired_snack[0])
+            desired_snack = desired_snack [1:]
+
+        else:
+            amount = 1
+            desired_snack = desired_snack
+
+        # remove white space around the snack
+        desired_snack = desired_snack.strip()
+
+        # check if snack is valid
+        snack_choice = string_check(desired_snack, valid_snacks)
+        
+        # check snack amount is valid (less than 5)
+        if amount >= 5:
+            print ("Sorry - We have a four snack maximum")
+            snack_choice = "invalid choice"
+
+        # add snack and amount to list...
+        snack_row.append (amount)
+        snack_row.append (snack_choice)
+
+        # check that snack is not the exit code before adding
+        if snack_choice != "xxx" and snack_choice != "invalid choice":
+            snack_order.append (snack_row)
+
+
 # ******  Main Routine  ********
+
+# valid options for yes / no questions
+_yes_no = [
+    ["yes", "y"],
+    ["no", 'n']
+]
 
 # initialise details loop so that it runs at least once
 MAX_TICKETS = 5
@@ -93,11 +187,24 @@ ticket_sales = 0
 all_names = []
 all_tickets = []
 
+
+popcorn = []
+mms = []
+pita_chips = []
+water = []
+orange_juice = []
+
+snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
+
 # data frame dictionary
 movie_data_dict = {
     'Name': all_names,
-    'Ticket': all_tickets
-}
+    'Popcorn': popcorn,
+    'Water': water,
+    'Pita Chips': pita_chips,
+    'M&Ms': mms,
+    'Orange Juice': orange_juice
+    }
 
 # ask user if they have used the program before & show instructions
 
@@ -129,7 +236,29 @@ while name != "xxx" and ticket_count < MAX_TICKETS:
     all_names.append (name)
     all_tickets.append (ticket_price)
 
-  
+    # ask user if they want a snack
+    check_snack = "invalid choice"
+    while check_snack == "invalid choice":
+        want_snack = input ("Do you want to order snacks? ").lower()
+        check_snack = string_check (want_snack, _yes_no)
+
+    # if they say yes, ask what snacks they want (and add to our snack list)
+    if check_snack == "Yes":
+        snack_order = get_snack()
+
+    else:
+        snack_order = []
+
+   # assume no snakcs have been purchased...
+    for item in snack_lists:
+        item.append (0)
+
+    for item in snack_order:
+        if len (item) > 0:
+            to_find = (item [1])
+            amount = (item[0])
+            add_list = movie_data_dict [to_find]
+            add_list [-1] = amount
 
 # print details
 movie_frame = pandas.DataFrame (movie_data_dict)
---
+++ 09_surcharge.py
@@ -0,0 +1,64 @@
+import re
+import pandas
+
+# function goes here
+# warning: the response is returned ion title case
+def string_check (choice, options):
+
+    is_valid = ""
+    chosen = ""
+
+    for var_list in options:
+
+        # if the snaclk is in one of the lists, return the full statement
+        if choice in var_list:
+
+            # get full name of snack and put it
+            #in title case so it looks nice when outputted
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+        
+        # if the chosen option is not valid, set is_valid to no
+        else:
+            is_valid = "no"
+
+    # if the snack is not OK - ask question again
+    if is_valid == "yes":
+        return chosen
+    else:
+        return "invalid choice"
+
+
+# Main routine
+
+pay_method = [
+    ["cash", "ca"],
+    ["credit", "cr"]
+]
+
+# loop until exit code
+name = ""
+while name != "xxx":
+    name = input ("Name: ")
+    if name == "xxx":
+        break
+
+    # ask for payment method
+    how_pay = "invalid choice"
+    while how_pay == "invalid choice":
+        how_pay = input ("Please choose a payment method (CASH or CREDIT): ")
+        how_pay = string_check (how_pay, pay_method)
+
+
+    # ask for subtotal (for testing purposes)
+    subtotal = float (input("Sub total? $"))
+
+    if how_pay == "Credit":
+        surcharge = 0.05 * subtotal
+    else:
+        surcharge = 0
+
+    total = subtotal + surcharge
+
+    print ("Name: {} | Subtotal: ${: .2f} | Surcharge: ${: .2f} | Total Payable: ${: .2f}".format (name, subtotal, surcharge, total))
